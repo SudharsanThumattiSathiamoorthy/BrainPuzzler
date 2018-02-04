fa9e94261fdfda7da170c6a4e021fa13897189aa
Sudharsan Sathiamoorthy <ssathiamoort@vmware.com>
2018-02-04 22:16:37 +0530

    Fixed arraylist bug..

--- app/src/main/java/labs/achu/sudharsan/com/brainpuzzler/MainActivity.java
+++ app/src/main/java/labs/achu/sudharsan/com/brainpuzzler/MainActivity.java
@@ -1,13 +1,12 @@
 package labs.achu.sudharsan.com.brainpuzzler;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
     private Button startButton;
 
-    private List<Integer> awnserList = new ArrayList<>();
+    private List<Integer> answerList = new ArrayList<>();
 
     private int correctAnswerLocation;
 
-    public void hideButton(final View view) {
-        startButton.setVisibility(View.INVISIBLE);
+    public void changeToGameLayout(final View view) {
+        this.startButton.setVisibility(View.INVISIBLE);
 
         setContentView(R.layout.game_layout);
 
@@ -33,24 +32,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setNextQuestion() {
-        final Random random = new Random();
-        int firstNo = random.nextInt(49);
-        int secondNo = random.nextInt(49);
+        // Clear the existing entries in answer list.
+        this.answerList.clear();
 
-        TextView questionTextView = findViewById(R.id.questionTextView);
+
+        final Random random = new Random();
+        final int firstNo = random.nextInt(49);
+        final int secondNo = random.nextInt(49);
+
+        final TextView questionTextView = findViewById(R.id.questionTextView);
 
         questionTextView.setText(Integer.toString(firstNo) + " + " + Integer.toString(secondNo));
 
-        Button resultButton1 = findViewById(R.id.result_button_1);
-        Button resultButton2 = findViewById(R.id.result_button_2);
-        Button resultButton3 = findViewById(R.id.result_button_3);
-        Button resultButton4 = findViewById(R.id.result_button_4);
+        final Button resultButton1 = findViewById(R.id.result_button_1);
+        final Button resultButton2 = findViewById(R.id.result_button_2);
+        final Button resultButton3 = findViewById(R.id.result_button_3);
+        final Button resultButton4 = findViewById(R.id.result_button_4);
 
         correctAnswerLocation = random.nextInt(4);
 
         for (int i = 0; i < 4; i++) {
             if (i == correctAnswerLocation) {
-                awnserList.add(firstNo + secondNo);
+                answerList.add(firstNo + secondNo);
             } else {
 
                 int incorrectAnswer = random.nextInt(98);
@@ -59,14 +62,14 @@ public class MainActivity extends AppCompatActivity {
                     incorrectAnswer = random.nextInt(98);
                 }
 
-                awnserList.add(incorrectAnswer);
+                answerList.add(incorrectAnswer);
             }
         }
 
-        resultButton1.setText(Integer.toString(awnserList.get(0)));
-        resultButton2.setText(Integer.toString(awnserList.get(1)));
-        resultButton3.setText(Integer.toString(awnserList.get(2)));
-        resultButton4.setText(Integer.toString(awnserList.get(3)));
+        resultButton1.setText(Integer.toString(answerList.get(0)));
+        resultButton2.setText(Integer.toString(answerList.get(1)));
+        resultButton3.setText(Integer.toString(answerList.get(2)));
+        resultButton4.setText(Integer.toString(answerList.get(3)));
     }
 
     public void verifyAnswer(final View view) {
@@ -99,6 +102,6 @@ public class MainActivity extends AppCompatActivity {
     public void startGame(final View view) {
         Log.i(MAIN_ACTIVITY, "Game is launched");
 
-        hideButton(view);
+        changeToGameLayout(view);
     }
 }
